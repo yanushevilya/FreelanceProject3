daad7bb2b8d2217300a87e5fb1005635398f6ccb
Максим <max.samurai.04@gmail.com>
2021-06-08 00:03:06 +0300

    Start working and crate recyclerView

---
+++ app/src/main/java/com/example/freelanceproject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.freelanceproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView numbersList;
+    private NumbersAdapter numbersAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        numbersList = findViewById(R.id.rv_numbers);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        numbersList.setLayoutManager(layoutManager);
+
+        numbersList.setHasFixedSize(true);
+
+        numbersAdapter = new NumbersAdapter(100);
+        numbersList.setAdapter(numbersAdapter);
+    }
+}
